01513975b5d625693937748d0ebfbd8cd8a8b869
yalerooo <Yaleros2@gmail.com>
2024-03-19 20:42:36 +0100

    BTS added

---
+++ src/main/java/net/mcreator/minekpop/item/BtsYouNeverWalkAloneItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.minekpop.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.network.chat.Component;
+
+import net.mcreator.minekpop.procedures.BtsYouNeverWalkAlonePlayerFinishesUsingItemProcedure;
+
+import java.util.List;
+
+public class BtsYouNeverWalkAloneItem extends Item {
+	public BtsYouNeverWalkAloneItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(0).saturationMod(0f).alwaysEat().build()));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.BOW;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 20;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+	}
+
+	@Override
+	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+		ItemStack retval = super.finishUsingItem(itemstack, world, entity);
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		BtsYouNeverWalkAlonePlayerFinishesUsingItemProcedure.execute(world, x, y, z);
+		return retval;
+	}
+}
